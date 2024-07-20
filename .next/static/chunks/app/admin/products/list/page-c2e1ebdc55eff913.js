f0fc7124e0d98495ed396ba2d81906205984270b
ashwin-pillai-aot <ashwin.pilai@algoocean.com>
2024-07-20 13:08:44 +0530

    Stop tracking .next folder

--- .next/static/chunks/app/admin/products/list/page-c2e1ebdc55eff913.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6156],{5154:function(e,n,s){Promise.resolve().then(s.t.bind(s,4712,23)),Promise.resolve().then(s.t.bind(s,7477,23))}},function(e){e.O(0,[4712,7477,2704,5801,1744],function(){return e(e.s=5154)}),_N_E=e.O()}]);
