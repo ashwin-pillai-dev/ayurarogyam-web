64ab3daad185d9b4a3e7fd92f1610e8243cde27a
ashwin-pillai-aot <ashwin.pilai@algoocean.com>
2024-07-25 04:43:42 +0530

    partner api changes

--- app/api/partners/route.ts
+++ app/api/partners/route.ts
@@ -1,5 +1,7 @@
 // pages/api/agents.js
 import prisma from '../../../lib/prisma';
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 
 export async function GET(request: Request) {
