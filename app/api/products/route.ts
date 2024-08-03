3de14a7e3607f26ba3bb3cb1c9915a9fe973e7b0
ashwin-pillai-aot <ashwin.pilai@algoocean.com>
2024-08-03 09:46:32 +0530

    revalidate 0 added to route files

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,9 @@
 // pages/api/agents.js
 import prisma from '../../../lib/prisma';
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 
 export async function GET(request: Request) {
 
